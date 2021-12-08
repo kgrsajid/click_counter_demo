2bb369b029282d0c52788534c484df6b9bfe45a2
FlutLab.io <>
2021-12-08 15:08:00 +0000

    Autosave

--- lib/view/myscreen.dart
+++ lib/view/myscreen.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends StateMVC<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$con.counter',
+              '${con.counter}',
               style: TextStyle(fontSize: 25),
             ),
           ],
